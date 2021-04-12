ecf5e6d2948117f1ca3a4630464c57cfe250289d
drtierney <d.r.tierney@hotmail.co.uk>
2021-04-12 19:20:24 +0100

    Stage #4 completed
    Stage 4/5: The Soup is Real

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,13 +1,30 @@
+import string
 import requests
 
-file = open("source.html", "wb")
-url = input("Input the URL:\n")
-r = requests.get(url)
-status_code = r.status_code
-if status_code == 200:
-    file.write(r.content)
-    print("Content saved.")
-else:
-    print(f"The URL returned {status_code}!")
+from bs4 import BeautifulSoup
 
-file.close()
+saved_articles = []
+translator = str.maketrans(' ', '_', string.punctuation)
+
+url = 'https://www.nature.com/nature/articles'
+response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+articles = soup.find_all('article')
+
+for article in articles:
+    article_type = article.find("span", attrs={'data-test': 'article.type'}).text.strip()
+    if article_type == 'News':
+        article_details = article.find("a", attrs={'data-track-action': 'view article'})
+        article_title = article_details.text
+        article_link = 'https://www.nature.com' + article_details['href']
+        article_filename = article_title.translate(translator).strip() + '.txt'
+        article_response = requests.get(article_link, headers={'Accept-Language': 'en-US,en;q=0.5'})
+        article_soup = BeautifulSoup(article_response.content, 'html.parser')
+        article_content = article_soup.find('div', class_='article__body').text.strip().encode()
+        with open(article_filename, 'wb') as file:
+            file.write(article_content)
+            saved_articles.append(article_filename)
+
+print(f"Saved articles:\n {saved_articles}")
